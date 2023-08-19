ccd2cc1f0835fec6ee6e1911c51c859b31fabecf
Boone Raye Flores <47687244+booneraye@users.noreply.github.com>
2023-08-20 02:29:02 +0800

    added portfolio basic infos

---
+++ src/data/basic-info.js
@@ -0,0 +1,157 @@
+import Image1 from "../assets/images/1.png";
+import { ReactComponent as Facebook } from "../assets/icons/facebook.svg";
+import { ReactComponent as LinkedIn } from "../assets/icons/linkedin.svg";
+import { ReactComponent as GitHub } from "../assets/icons/github.svg";
+import GitHub1 from "../assets/icons/github1.svg";
+import Nodejs from "../assets/icons/nodejs.svg";
+import ReactJS from "../assets/icons/reactjs.svg";
+import HTML from "../assets/icons/html.svg";
+import CSS from "../assets/icons/css.svg";
+import PHP from "../assets/icons/php.svg";
+import Javascript from "../assets/icons/javascript.svg";
+import Mysql from "../assets/icons/mysql.svg";
+import Wordpress from "../assets/icons/wordpress.svg";
+import Woocommerce from "../assets/icons/woocommerce.svg";
+import Linux from "../assets/icons/linux.svg";
+import GoogleAnalytics from "../assets/icons/google-analytics.svg";
+import MicrosoftOffice from "../assets/icons/microsoft.svg";
+import MSAdmin from "../assets/icons/ms-admin.svg";
+import AdobeXD from "../assets/icons/xd.svg";
+
+const INFO = {
+  NAME: "Boone Raye Flores",
+  EMAIL_ADDRESS: "booneraye@gmail.com",
+  SOCIALS: [
+    {
+      label: "Facebook",
+      link: "https://www.facebook.com/booneraye",
+      Logo: Facebook,
+    },
+    {
+      label: "LinkedIn",
+      link: "https://www.linkedin.com/in/booneraye/",
+      Logo: LinkedIn,
+    },
+    { label: "Github", link: "https://github.com/booneraye", Logo: GitHub },
+  ],
+  ROLES: [
+    "Front-end Developer",
+    "Web Developer",
+    "IT Specialist",
+    "Social Media Manager",
+    "Digital Marketer",
+    "SEO Specialist",
+  ],
+  INTRODUCTION: (
+    <>
+      <p>
+        I'm a career shifter who previously worked in sales and marketing. My
+        responsibilities included social media management, content creation, and
+        merchant and partner acquisition.
+      </p>
+      <p>
+        However, I decided to pursue my passion and dream of becoming a web
+        developer in 2020. The transition was difficult but I'm glad to be on
+        this path. I'm still learning but I have already accomplished some
+        projects that I'm proud of.
+      </p>
+      <p>
+        <b>
+          I'm dedicated to continuing my growth and development as a web
+          developer.
+        </b>
+      </p>
+    </>
+  ),
+  SKILLS: [
+    {
+      stack: "ReactJS",
+      Logo: ReactJS,
+      description:
+        "Experienced in ReactJs fro creating single-page applications.",
+    },
+    {
+      stack: "Nodejs",
+      Logo: Nodejs,
+      description: "Experienced in using Nodejs for backend and creating API",
+    },
+    {
+      stack: "Javascript",
+      Logo: Javascript,
+      description: "Experience in Javascript for making web applications",
+    },
+    {
+      stack: "Mysql",
+      Logo: Mysql,
+      description:
+        "Experienced in using MySQL and SQL for database management and querying.",
+    },
+    {
+      stack: "Wordpress",
+      Logo: Wordpress,
+      description:
+        "Experienced in using WordPress and Woocommerce for creating and managing websites and online stores.",
+    },
+    {
+      stack: "Woocommerce",
+      Logo: Woocommerce,
+      description:
+        "Experienced in using WordPress and Woocommerce for creating and managing websites and online stores.",
+    },
+    {
+      stack: "Linux",
+      Logo: Linux,
+      description: "Experince in utilizing Linux OS enviroment",
+    },
+    {
+      stack: "GitHub",
+      Logo: GitHub1,
+      description:
+        "Experienced in using GitHub for version control and collaboration.",
+    },
+    {
+      stack: "HTML",
+      Logo: HTML,
+      description: "Proficient in HTML for creating web pages.",
+    },
+    {
+      stack: "CSS",
+      Logo: CSS,
+      description: "Proficient in CSS for styling web pages.",
+    },
+    {
+      stack: "PHP",
+      Logo: PHP,
+      description: "Experience in PHP for back-end and server-side development",
+    },
+    {
+      stack: "AdobeXD",
+      Logo: AdobeXD,
+      description:
+        "Experience in AdobeXD in making UI design and wireframes for mockup website",
+    },
+    {
+      stack: "Microsoft Office",
+      Logo: MicrosoftOffice,
+      description: "Experience in various MS Office applications",
+    },
+    {
+      stack: "MS Admin",
+      Logo: MSAdmin,
+      description:
+        "Experience in MS Office Admin Dashboard for manageing active directory fro emails and more",
+    },
+    {
+      stack: "Google Analytics",
+      Logo: GoogleAnalytics,
+      description:
+        "Experience in utilizing google analytics dashboard for monitoring and generating reports.",
+    },
+  ],
+  PORTFOLIO: {
+    COLORS: { primary: "#ce2c5a", secondary: "#E24D24" },
+    NAME: "Portfolio 101",
+  },
+};
+
+export { INFO };
